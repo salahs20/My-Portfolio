348e7f456cadf517c5fe5aa41c38809ffd89846d
Salah Mohamed <salahmohamedalzeini@gmail.com>
2024-11-10 14:44:41 +0200

    update cv

--- src/Component/Pages/CV.jsx
+++ src/Component/Pages/CV.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FaDownload } from 'react-icons/fa'; // For download icon
+import cv from '../../Image/Salah-Mohamed-Resume-1.png'
 const CV = () => {
+  const pdfUrl="https://drive.google.com/file/d/1I_dMYExCcPxr7VnNBSjHweadfevOEHEH/view?usp=drivesdk"
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 p-6 pt-[5.5rem]">
       {/* Card container with centered content */}
@@ -10,28 +12,21 @@ const CV = () => {
         </h1>
         
         {/* PDF Viewer Section */}
-        <div className="pdf-container" style={{ height: "100vh", width: "100%" }}>
+        <div className="pdf-container">
           
-        <iframe
-        src="https://drive.google.com/file/d/1I_dMYExCcPxr7VnNBSjHweadfevOEHEH/preview"
-        title="PDF Viewer"
-        width="100%"
-        height="100%"
-        style={{ border: "none" }}
-      />
-       
+       <a href="https://drive.google.com/file/d/1I_dMYExCcPxr7VnNBSjHweadfevOEHEH/view?usp=drivesdk"> <img src={cv} alt="My CV" /> </a>
         </div>
 
         {/* Download Button */}
         <div className="flex justify-center mt-6">
-          <a
-            href="https://drive.google.com/file/d/1I_dMYExCcPxr7VnNBSjHweadfevOEHEH/view?usp=drivesdk" // Make sure the path is correct for download
-            download
-            className="flex items-center bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-blue-700 transition-all"
-          >
-            <FaDownload className="mr-2" /> Download CV
-          </a>
-        </div>
+      <a
+        href={pdfUrl} // رابط التحميل المباشر
+        download="My_CV.pdf" // اسم الملف الذي سيتم تحميله
+        className="flex items-center bg-blue-600 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-blue-700 transition-all"
+      >
+        <FaDownload className="mr-2" /> Download CV
+      </a>
+    </div>
       </div>
     </div>
   );
